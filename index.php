c0e3cda1a00c10356e896330a7ab7f6d81215d95
HellFireInfernoStorm <dinura2003@gmail.com>
2020-12-18 08:48:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Home</title>
+  <link rel="stylesheet" href="styles/topnav.css">
+  <link rel="stylesheet" href="styles/home.css">
+</head>
+
+<body style="background-color: #000">
+
+  <nav class="topnav">
+    <a class="in_use" href="index.php">Home</a>
+    <a href="">MenuItem</a>
+    <a href="">MenuItem</a>
+    <a href="">MenuItem</a>
+    <a href="pages/login.php" id="login_button">Login</a>
+  </nav>
+
+  <div class="home_body">
+    <h1 id="topic">New Arrivals</h1>
+
+    <div class="home_links">
+      <a href="">Something</a>
+      <a href="">Something</a>
+      <a href="">Something</a>
+      <a href="">Something</a>
+      <a href="">Something</a>
+      <a href="">Something</a>
+    </div>
+  </div>
+
+</body>
+
+</html>
