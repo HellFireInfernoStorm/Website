03f7c9f91602d86bf0e4eef8bce087e7e8e623c9
HellFireInfernoStorm <dinura2003@gmail.com>
2020-12-18 10:43:14 +0530

    commit

--- resources/header.php
+++ resources/header.php
@@ -1,4 +1,4 @@
-<nav class="topnav" onload="current_tab(<?php echo $current ?>)">
+<nav class="topnav" onload="current_tab()">
   <a id="home_button" href="../index.php">Home</a>
   <a href="">MenuItem</a>
   <a href="">MenuItem</a>
@@ -7,7 +7,7 @@
 </nav>
 
 <script>
-  function current_tab(current){
+  function current_tab(){
       if (current == "home") {
         document.getElementById("home_button").className = "in_use";
       }
