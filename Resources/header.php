9b7fc0a18132cccea1c5e3163133a81f7eae4be3
HellFireInfernoStorm <dinura2003@gmail.com>
2020-12-18 09:27:56 +0530

    Update header.php

--- Resources/header.php
+++ Resources/header.php
@@ -1,7 +1,7 @@
 <nav class="topnav">
-    <a class="in_use" href="index.php">Home</a>
+    <a class="in_use" href="../index.php">Home</a>
     <a href="">MenuItem</a>
     <a href="">MenuItem</a>
     <a href="">MenuItem</a>
-    <a href="pages/login.php" id="login_button">Login</a>
+    <a href="pages/login.php" id="../pages/login_button">Login</a>
 </nav>
