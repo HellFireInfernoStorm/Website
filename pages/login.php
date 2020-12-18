9b9c3b4fa98d05260c4928f453dc227aaf6a93fd
HellFireInfernoStorm <dinura2003@gmail.com>
2020-12-18 08:48:30 +0530

    Add files via upload

---
+++ pages/login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Login</title>
+  <link rel="stylesheet" href="../styles/topnav.css">
+</head>
+
+<body style="background-color: #000">
+
+  <div class="topnav">
+    <a href="../index.php">Home</a>
+    <a href="">MenuItem</a>
+    <a href="">MenuItem</a>
+    <a href="">MenuItem</a>
+	  <a class="in_use" id="login_button" href="login.php">Login</a>
+  </div>
+  
+  <div class="login_form">
+    <form>
+
+	  </form>
+  </div>
+</body>
+</html>
