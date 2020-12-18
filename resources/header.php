662109c07796705fa17f07b9c8fa0ba2aff60766
HellFireInfernoStorm <dinura2003@gmail.com>
2020-12-18 09:52:19 +0530

    commit

---
+++ resources/header.php
@@ -0,0 +1,7 @@
+<nav class="topnav">
+    <a href="../index.php">Home</a>
+    <a href="">MenuItem</a>
+    <a href="">MenuItem</a>
+    <a href="">MenuItem</a>
+    <a href="../pages/login.php" id="login_button">Login</a>
+</nav>
