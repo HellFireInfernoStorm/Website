9662b691347ad003caa1ea071c8a87e26f20c129
HellFireInfernoStorm <dinura2003@gmail.com>
2020-12-18 12:29:38 +0530

    commit

---
+++ resources/config.php
@@ -0,0 +1,12 @@
+<?php
+  $DB_Server = "localhost";
+  $DB_User = "root";
+  $DB_Pass = "";
+  $DB_Name = "website";
+
+  $DB = mysqli_connect($DB_Server, $DB_User, $DB_Pass, $DB_Name);
+
+  if ($DB === false) {
+    die("Error : Connection Error." . mysqli_connect_error())
+  }
+?>
