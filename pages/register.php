b4368022b1bd51be9512c7df7a7c2f40193dc945
HellFireInfernoStorm <dinura2003@gmail.com>
2020-12-25 04:34:06 +0530

    commit

--- pages/register.php
+++ pages/register.php
@@ -4,21 +4,19 @@
 <head>
   <meta charset="UTF-8">
   <title>Register</title>
-  <link rel="stylesheet" href="../styles/topnav.css">
   <link rel="stylesheet" href="../styles/register.css">
 </head>
 
-<body style="background-color: #000;" onload="current_tab()">
+<body style="background-color: #000;">
 
-  <script>
-    var current = "register";
-  </script>
-
-  <?php include '../resources/header.php'; ?>
-
-  <h1 id="topic">Create a new Account</h1>
-  <form action="authenticate.php" method="post">
-    
+  <h1 id="topic">Sign Up</h1>
+  <form action="authenticate.php" method="post" id="register_form">
+    <label for="username">Enter a Username</label><br>
+    <input type="text" name="username" id="username" required><br>
+    <label for="password">Enter a Password</label><br>
+    <input type="password" name="password" id="password"><br>
+    <label for="confirm_password">Confirm Password</label><br>
+    <input type="password" name="confirm_password" id="confirm_password">
   </form>
 
 </body>
