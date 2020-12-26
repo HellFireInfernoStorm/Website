2f444502206e4af348a4ab7c8028760b4914b6b1
HellFireInfernoStorm <dinura2003@gmail.com>
2020-12-26 15:45:07 +0530

    commit

--- pages/register.php
+++ pages/register.php
@@ -14,13 +14,15 @@
     <label for="username">Enter a Username</label><br>
     <input type="text" name="username" id="username" required><br>
     <label for="password">Enter a Password</label><br>
-    <input type="password" name="password" id="password"><br>
+    <input type="password" name="password" id="password" required><br>
     <label for="confirm_password">Confirm Password</label><br>
-    <input type="password" name="confirm_password" id="confirm_password">
+    <input type="password" name="confirm_password" id="confirm_password" required>
+    <input type="submit" value="Submit">
   </form>
 
   <div class="container">
     <a href="login.php" id="back_button">Back</a>
   </div>
 </body>
+
 </html>
--- resources/config.php
+++ resources/config.php
@@ -1,12 +1,12 @@
 <?php
-  $DB_Server = "localhost";
-  $DB_User = "root";
-  $DB_Pass = "";
-  $DB_Name = "website";
+$DB_Server = "localhost";
+$DB_User = "root";
+$DB_Pass = "";
+$DB_Name = "website";
 
-  $link = mysqli_connect($DB_Server, $DB_User, $DB_Pass, $DB_Name);
+$link = mysqli_connect($DB_Server, $DB_User, $DB_Pass, $DB_Name);
 
-  if ($link === false) {
-    die("Error : Connection Error." . mysqli_connect_error());
-  }
+if ($link === false) {
+  die("Error : Connection Error." . mysqli_connect_error());
+}
 ?>
